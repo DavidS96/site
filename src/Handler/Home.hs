46a4b71a1cd0cccbd6f518232e2ef20305b9a8f0
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-10-25 00:57:58 +0000

    OLA SITE

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,13 +1,32 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
+--import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    toWidgetHead [julius|
+        function ola(){
+            alert("ola");
+        }
+    |]
+    toWidgetHead [lucios|
+       h1{
+           color : red;
+           }
+       }]
+       
+       [whamlet|
+       <h1>
+            OLA MUNDO!
+            
+       <BUTTON ONCLICK="OLA()">
+            OLA
+        }]
