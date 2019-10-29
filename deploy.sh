a8c1f23ce4c34eede726430d9afa0481a618bede
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-10-29 22:42:57 +0000

    MeuDeploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ git add . &&
 git commit -m $1 &&
 git push origin master &&
 ssh root@164.132.194.102 <<EOF
-cd aulahaskadms &&
+cd site &&
 git pull origin master &&
 stack build &&
 lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
 nohup stack exec aulahaskell > /dev/null
-echo "deploy finishedsl"
+echo "deploy finished"
 EOF
