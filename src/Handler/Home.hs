51243946c8007a0aa1a5b621bc76cb88039d4f03
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-11-09 03:09:18 +0000

    vamos

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,86 +1,60 @@
-<html>
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="description" content="">
-    <meta name="keywords" content="pokemon, torneio, pokesquad, tutorial">
-    <meta name="author" content="Daniel 'Yzumaru' Campos">
-    <title>PokeSquad | Home</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" type="text/css" href="../css/slick.css">
-</head>
+import Import
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
 
-<body>
-    <header>
-        <div class="container">
-            <a href="index.html">
-                <div id="logo"></div>
-            </a>
-            <nav>
-                <ul>
-                    <li class="current"><a href="index.html">Home</a></li>
-                    <li><a href="events.html">Events</a></li>
-                    <li><a href="rules.html">Rules</a></li>
-                    <li><a class="login" href="#" id="button">Sign in</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    <div class="container">
-        <h1><span class="highlight">Eventos</span> Recentes</h1>
-        <section id="slider">
-            <div class="banner1"></div>
-            <div class="banner2"></div>
-    </div>
-    <section id="boxes">
-        <div class="container">
-            <div class="box">
-                <a href="https://discord.gg/bHEkvUM" target="_blank">
-                    <img src="../img/discord.png">
-                </a>
-                <h3>Servidor do Discord</h3>
-                <p>Conecte-se em nosso servidor para bater um papo enquanto batalha</p>
-            </div>
-            <div class="box">
-                <a href="https://chat.whatsapp.com/FBx7SodScQH2KhQt39D4sv" target="_blank">
-                    <img src="../img/whatsapp.png">
-                </a>
-                <h3>Grupo do Whatsapp</h3>
-                <p>Entre em nosso grupo e participe de todos os eventos e encontrar batalhas</p>
-            </div>
-            <div class="box">
-                <a href="https://www.facebook.com/PokeSquadLeague/" target="_blank">
-                    <img src="../img/facebook.png">
-                </a>
-                <h3>Página no Facebook</h3>
-                <p>Curta nossa página para sempre ficar ligado no que está por vir</p>
-            </div>
-        </div>
-    </section>
-    <footer>
-        <div class="container">
-            <p>PokeSquad LEAGUE &trade; | Grupo de Pokemon voltado sempre para as ultimas gerações, foca em entregar um
-                modo novo de se jogar fora das convenções já estabelecidas por formatos como Smogon e VGC, oferecendo um
-                formato
-                menos competitivo e mais <i>for fun</i>.</p>
-        </div>
-    </footer>
-    <div class="modal-holder">
-        <div class="modal-content">
-            <div class="close">+</div>
-            <form class="txtbox login" action="index.html" method="post">
-                <input type="text" name="" placeholder="Email">
-                <input type="password" name="" placeholder="Senha">
-                <input type="submit" name="" value="Login">
-                <div id="signup"><a href="signup.html">cadastre-se</a></div>
-            </form>
-        </div>
-    </div>
-    <script type="text/javascript" src="../js/jquery.js"></script>
-    <script type="text/javascript" src="../js/jquery-migrate.js"></script>
-    <script type="text/javascript" src="../js/slick.min.js"></script>
-    <script type="text/javascript" src="../js/main.js"></script>
-</body>
+getPage1R ::Handler Html
+getPage1R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page1.hamlet")
+        toWidgetHead $(luciusFile "templates/page1.lucius")
+        toWidgetHead $(juliusFile "templates/page1.julius")
 
-</html>
+getPage2R ::Handler Html
+getPage2R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page2.hamlet")
+
+getPage3R ::Handler Html
+getPage3R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page3.hamlet")
+        toWidgetHead $(luciusFile "templates/page3.lucius")
+        toWidgetHead $(juliusFile "templates/main.julius")
+        toWidgetHead $(juliusFile "templates/jquery.julius")
+        toWidgetHead $(juliusFile "templates/jquery-migrate.julius")
+        toWidgetHead $(juliusFile "templates/slick.min.julius")
+
+
+getHomeR :: Handler Html
+getHomeR = do 
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead [julius|
+            function ola(){
+                alert("ola");
+            }
+        |]
+        toWidgetHead [lucius|
+            h1 {
+                color : red;
+            }
+        |]
+        [whamlet|
+            <h1>
+                OLA MUNDO!
+            
+            <img src=@{StaticR sample_png}>
+            
+            <button class="btn btn-danger" onclick="olaa">
+                OLA
+        |]
