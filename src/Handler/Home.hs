3b08b4d0b6d47ad2106720b7e308ebb78a94d8f2
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-11-09 03:18:59 +0000

    vamos

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -29,8 +29,8 @@ getPage3R = do
     defaultLayout $ do
         $(whamletFile "templates/page3.hamlet")
         toWidgetHead $(luciusFile "templates/page3.lucius")
-        toWidgetHead $(juliusFile "templates/main.julius")
---        toWidgetHead $(juliusFile "templates/jquery.julius")
+--        toWidgetHead $(juliusFile "templates/main.julius")
+        toWidgetHead $(juliusFile "templates/jquery.julius")
 --        toWidgetHead $(juliusFile "templates/jquery-migrate.julius")
 --        toWidgetHead $(juliusFile "templates/slick.min.julius")
 
