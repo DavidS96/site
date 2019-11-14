e1cd4efafb5c88486fdbc22bfa9fe6864621820a
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-11-14 00:35:24 +0000

    teste3

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -12,29 +12,27 @@ import Database.Persist.Postgresql
 import Text.Lucius
 import Text.Julius
 
-getPage1R ::Handler Html
-getPage1R = do
-    defaultLayout $ do
-        $(whamletFile "templates/page1.hamlet")
-        toWidgetHead $(luciusFile "templates/page1.lucius")
-        toWidgetHead $(juliusFile "templates/page1.julius")
-
-getPage2R ::Handler Html
-getPage2R = do
-    defaultLayout $ do
-        $(whamletFile "templates/page2.hamlet")
-
 getHomeR ::Handler Html
 getHomeR = do
     defaultLayout $ do
-    <meta name="viewport" content="width=device-width">
-        --toWidget $(jsFile "templates/slick.min.js")
-        --toWidget $(jsFile "templates/main.js")
+        toWidget $(jsFile "templates/slick.min.js")
+        toWidget $(jsFile "templates/main.js")
         --addScript (StaticR slick_js)
         --addScript (StaticR main_js)
         toWidget $(luciusFile "templates/style.lucius")
         $(whamletFile "templates/index.hamlet")
 
+-- getPage1R ::Handler Html
+-- getPage1R = do
+--     defaultLayout $ do
+--         $(whamletFile "templates/page1.hamlet")
+--         toWidgetHead $(luciusFile "templates/page1.lucius")
+--         toWidgetHead $(juliusFile "templates/page1.julius")
+
+-- getPage2R ::Handler Html
+-- getPage2R = do
+--     defaultLayout $ do
+--         $(whamletFile "templates/page2.hamlet")
 
 -- getHomeR :: Handler Html
 -- getHomeR = do 
