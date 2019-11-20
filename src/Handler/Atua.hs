2831255a51d87697a628fb71b9334466bf310ad0
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-11-20 03:38:01 +0000

    teste69lmao

--- src/Handler/Atua.hs
+++ src/Handler/Atua.hs
@@ -1,49 +1,81 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Atua where
 
 import Import
---import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
 import Text.Lucius
 import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
 
-formUsu :: Form Ator
-formUsu = renderBootstrap $ Ator 
-    <$> areq textField "Nome: " Nothing
-    <*> areq emailField "Nasc: " Nothing
-  
-getUsuarioR :: Handler Html
-getUsuarioR = do 
-    (widget,enctype) <- generateFormPost formUsu
-    defaultLayout $ do 
-        msg <- getMessage
+serieCB = do
+  rows <- runDB $ selectList [] [Asc SerieNome]
+  optionsPairs $ 
+      map (\r -> (serieNome $ entityVal r, entityKey r)) rows
+
+atorCB = do
+  rows <- runDB $ selectList [] [Asc AtorNome]
+  optionsPairs $ 
+      map (\r -> (atorNome $ entityVal r, entityKey r)) rows
+
+-- renderDivs
+formAtua :: Form Atua 
+formAtua = renderBootstrap $ Atua
+    <$> areq (selectField serieCB) "Serie: " Nothing
+    <*> areq (selectField atorCB) "Ator: " Nothing
+    <*> areq intField "Ano: " Nothing
+
+    
+getAtuaR :: Handler Html
+getAtuaR = do 
+    (widget,_) <- generateFormPost formAtua
+    msg <- getMessage
+    defaultLayout $ 
         [whamlet|
-            $maybe mensa <- msg
+            $maybe mensa <- msg 
                 <div>
                     ^{mensa}
             
             <h1>
-                CADASTRO DE Usuario
-                
-            <form method=post action=@{UsuarioR}>
+                CADASTRO DE ATUACOES
+            
+            <form method=post action=@{AtuaR}>
                 ^{widget}
                 <input type="submit" value="Cadastrar">
         |]
 
-postUsuarioR :: Handler Html
-postUsuarioR = do 
-    ((result,_),_) <- runFormPost formUsu
+postAtuaR :: Handler Html
+postAtuaR = do 
+    ((result,_),_) <- runFormPost formAtua
     case result of 
-        FormSuccess usuario -> do 
-            runDB $ insert usuario
+        FormSuccess atua -> do 
+            runDB $ insert atua 
             setMessage [shamlet|
-                <h2>
-                    USUARIO INSERIDO COM SUCESSO
+                <div>
+                    ATUACAO INCLUIDA
             |]
-            redirect UsuarioR
+            redirect AtuaR
         _ -> redirect HomeR
+
+getElencoR :: SerieId -> Handler Html
+getElencoR serieid = do 
+    let sql = "SELECT ??,??,?? FROM serie \
+          \ INNER JOIN atua ON atua.serieid = serie.id \
+          \ INNER JOIN ator ON atua.atorid = ator.id \
+          \ WHERE serie.id = ?"
+    serie <- runDB $ get404 serieid
+    atores <- runDB $ rawSql sql [toPersistValue serieid] :: Handler [(Entity Serie,Entity Atua,Entity Ator)]
+    defaultLayout $ do 
+        [whamlet|
+            <h1>
+                ELENCO DE #{serieNome serie}
+            <ul>
+                $forall (Entity _ _, Entity _ _, Entity _ ator) <- atores
+                    <li>
+                        #{atorNome ator}
+        |]
+      
