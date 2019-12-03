4a12675edb553bb2e897ea3f75532bd7168ddb4c
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-12-03 02:00:30 +0000

    adm navegacao

--- src/Handler/Login.hs
+++ src/Handler/Login.hs
@@ -73,4 +73,65 @@ postLogoutR = do
 getAdminR :: Handler Html
 getAdminR = do 
     defaultLayout $ do
-       toWidget $(luciusFile "templates/style.lucius")
+      toWidget $(luciusFile "templates/style.lucius")
+        [whamlet|
+    <header>
+    <div class="container">
+        <a href=@{HomeR}>
+            <div id="logo">
+        <nav>
+            <ul>
+                <li class="current">
+                    <a href=@{HomeR}>Home
+                <li>
+                    <a href=@{EventsR}>Events
+                <li>
+                    <a href=@{RulesR}>Rules
+                $maybe nomeSess <- sess
+                    <li>
+                       <form method=post action=@{LogoutR}>
+                          <input type="submit" value="Sair">
+                    <div>
+                        Ola #{nomeSess}
+                $nothing
+                    <li>
+                    <a href=@{SigninR}>Sign in       
+                      
+
+<div class="container">
+    <h1>
+        <span class="highlight">Eventos</span> Recentes
+    <div id="slider">
+
+<section id="boxes">
+    <div class="container">
+        <div class="box">
+            <a href="https://discord.gg/bHEkvUM" target="_blank">
+                <img src=@{StaticR discord_png}>
+
+            <h3>Servidor do Discord
+            <p>Conecte-se em nosso servidor para bater um papo enquanto batalha
+
+        <div class="box">
+            <a href="https://chat.whatsapp.com/FBx7SodScQH2KhQt39D4sv" target="_blank">
+                <img src=@{StaticR whatsapp_png}>
+
+            <h3>Grupo do Whatsapp
+            <p>Entre em nosso grupo e participe de todos os eventos e encontrar batalhas
+
+        <div class="box">
+            <a href="https://www.facebook.com/PokeSquadLeague/" target="_blank">
+                <img src=@{StaticR facebook_png}>
+
+            <h3>Página no Facebook
+            <p>Curta nossa página para sempre ficar ligado no que está por vir
+
+<footer>
+    <div class="container">
+        <p>PokeSquad LEAGUE &trade; | Grupo de Pokemon voltado sempre para as ultimas gerações, foca em entregar um 
+            modo novo de se jogar fora das convenções já estabelecidas por formatos como Smogon e VGC, oferecendo um
+            formato
+            menos competitivo e mais <i>for fun.
+    
+    |]
+    
