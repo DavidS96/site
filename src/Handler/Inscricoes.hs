7fa3b49680ee2764622dc8da2dcae44f868a7316
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-12-03 22:17:42 +0000

    box!

---
+++ src/Handler/Inscricoes.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Inscricoes where
+
+import Import
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+eventoCB = do
+   rows <- runDB $ selectList [] [Asc EventoNome]
+   optionsPairs $ 
+      map (\r -> (eventoNome $ entityVal r, entityKey r)) rows
+
+ --renderDivs
+formInscricoes :: UsuarioId -> Form Inscricoes
+formInscricoes userId = renderBootstrap $ (Inscricoes userId)
+      <$> areq (selectField eventoCB) "evento : " Nothing
+
+    
+getInscricoesR :: Handler Html
+getInscricoesR = do
+     
+     nome <- lookupSession "_NOME"
+     x <- runDB $ selectFirst [UsuarioNome =. nome] []
+     let key = case x of
+                  Just (Entity key _) -> key
+    
+     (widget,_) <- generateFormPost (formInscricoes key)
+     msg <- getMessage
+     defaultLayout $ 
+         [whamlet|
+             $maybe mensa <- msg 
+                 <div>
+                     ^{mensa}
+            
+             <h1>
+                 Inscricao
+            
+             <form method=post action=@{InscricoesR}>
+                 ^{widget}
+                <input type="submit" value="Cadastrar">
+         |]
+
+postInscricoesR :: Handler Html
+postInscricoesR = do 
+    ((result,_),_) <- runFormPost formInscricoes
+    case result of 
+        FormSuccess inscricoes -> do 
+            runDB $ insert inscricoes
+            setMessage [shamlet|
+                <div>
+                    Inscrito INCLUIDA
+            |]
+            redirect InscricoesR
+        _ -> redirect HomeR
+
+getIncritoR :: EventoId -> Handler Html
+getIncritoR eventoid = do 
+    let sql = "SELECT ??,??,?? FROM evento \
+          \ INNER JOIN inscricoes ON inscricoes.eventoid = evento.id \
+          \ INNER JOIN usuario ON inscricoes.usuarioid = usuario.id \
+          \ WHERE evento.id = ?"
+    evento <- runDB $ get404 eventoid
+    inscrito <- runDB $ rawSql sql [toPersistValue eventoid] :: Handler [(Entity Evento,Entity Inscricoes,Entity Usuario)]
+    defaultLayout $ do 
+        [whamlet|
+            <h1>
+                ELENCO DE #{eventoNome evento}
+            <ul>
+                $forall (Entity _ _, Entity _ _, Entity _ usuario) <- inscrito
+                    <li>
+                        #{usuarioNome usuario}
+        |]
+      
