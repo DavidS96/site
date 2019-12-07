d8ee34f4fb18ced7110640e9ea68041770c4baf4
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-12-07 02:47:39 +0000

    inscricoes

--- src/Handler/Inscricoes.hs
+++ src/Handler/Inscricoes.hs
@@ -20,7 +20,7 @@ eventoCB = do
  --renderDivs
 formInscricoes :: UsuarioId -> Form Inscricoes
 formInscricoes userId = renderBootstrap $ (Inscricoes userId)
-      <$> areq (selectField eventoCB) "Evento : " Nothing
+      <$> areq (selectField eventoCB)  Nothing
 
     
 getInscricoesR :: Handler Html
