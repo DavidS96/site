1faeb94064928d61ae809976035448872dc31148
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2019-12-07 05:12:48 +0000

    admin

--- src/Handler/Login.hs
+++ src/Handler/Login.hs
@@ -10,7 +10,7 @@ module Handler.Login where
 import Import
 import Text.Lucius
 import Text.Julius
-import Handler.EventoR (formEvento)
+import Handler.EventoR 
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
